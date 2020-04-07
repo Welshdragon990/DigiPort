0bd0c4ad1e810d27e3a48820c3b287577b68fed6
Welshdragon990 <36169491+Welshdragon990@users.noreply.github.com>
2020-04-07 14:38:24 +0100

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,27 @@
+let name = "Karl Roberts"
+let age =  48
+let jobRole = "Service Delivery Manager"
+
+
+
+function personalData(name, age) {
+    console.log("Hello, I am " + name + " I am " + age + " years old" )
+
+}
+personalData("Jacob", 44)
+personalData("Dean", 52)
+personalData("Stuart", 91)
+
+
+    let numberOne;
+    let numberTwo;
+
+function addition(numberOne, numberTwo) {
+    let total = numberOne + numberTwo
+    console.log(total)
+}
+
+addition(50, 40)
+addition(30, 50)
+addition(500, 40)
+addition(300, 50)
